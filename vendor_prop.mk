e7f2bde2895c60a6d4adc9945b4fa92a4bae995c
XiNGRZ <chenxingyu92@gmail.com>
2020-01-26 15:09:08 +0800

    sdm845: Pick changes from stock
    Change-Id: Id237c3abcc8a876aeba6e8585ebf9f747c4fcbec

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,14 +2,16 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio.deep_buffer.media=true \
+    audio.offload.disable=true \
     audio.offload.video=true \
     persist.vendor.audio.fluence.speaker=true \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.ras.enabled=false \
     persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac \
+    persist.vendor.bt.splita2dp.44_1_war=true \
     ro.af.client_heap_size_kbyte=7168 \
-    ro.vendor.audio.sdk.fluencetype=none \
+    ro.vendor.audio.sdk.fluencetype=fluence \
     ro.vendor.audio.sdk.ssr=false \
     vendor.audio.adm.buffering.ms=2 \
     vendor.audio.dolby.ds2.enabled=false \
@@ -24,8 +26,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.offload.multiaac.enable=true \
     vendor.audio.offload.multiple.enabled=false \
     vendor.audio.offload.passthrough=false \
-    vendor.audio.offload.pstimeout.secs=3 \
-    vendor.audio.offload.track.enable=true \
+    vendor.audio.offload.pstimeout.secs=10 \
+    vendor.audio.offload.track.enable=false \
     vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.safx.pbe.enabled=true \
     vendor.audio.tunnel.encode=false \
@@ -33,7 +35,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.use.sw.ape.decoder=true \
     vendor.audio_hal.in_period_size=144 \
     vendor.audio_hal.period_multiplier=3 \
-    vendor.audio_hal.period_size=192 \
+    vendor.audio_hal.period_size=480 \
     vendor.fm.a2dp.conc.disabled=true \
     vendor.voice.path.for.pcm.voip=true \
 
@@ -43,11 +45,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapmaxfree=8m \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heaptargetutilization=0.75 \
+
+# GMS
+PRODUCT_GMS_CLIENTID_BASE := android-meizu
 
 # Misc
 PRODUCT_PROPERTY_OVERRIDES += \
